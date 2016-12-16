26abf7f0404524191af15deb98e4c3901a6f2ab8
Natchairn <ma.natcharin_st@tni.ac.th>
2016-12-16 02:42:02 +0000

    First commit

---
+++ hello-cpp-world.cc
@@ -0,0 +1,17 @@
+#include <iostream>
+
+class PrintLotto{
+    int run_1st();
+    int lotto_1st;
+    
+};
+
+int main() {
+    std::cout << "Hello World!" << std::endl;
+}
+int run_1st()
+{ int x;
+
+    
+    return 
+}
